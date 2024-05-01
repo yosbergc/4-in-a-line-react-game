8ec20d7fbd2edb628735ef32a1bf0b276df486c5
Yosber Guerrero <yosbergc@gmail.com>
2024-05-01 09:05:10 -0400

    Added all the combos

--- src/components/Game/Board.jsx
+++ src/components/Game/Board.jsx
@@ -4,14 +4,20 @@ import React from 'react'
 
 const initialBoard = Array(16).fill(null)
 const turns = {
-  player1: 'X',
-  player2: 'Y'
+  player1: '❌',
+  player2: '🔵'
 }
 const WINNER_COMBOS = [
   [0, 1, 2, 3],
   [4, 5, 6, 7],
   [8, 9, 10, 11],
-  [12, 13, 14, 15]
+  [12, 13, 14, 15],
+  [0, 4, 8, 12],
+  [1, 5, 9, 13],
+  [2, 6, 10, 14],
+  [3, 7, 11, 15],
+  [0, 5, 10, 15],
+  [3, 6, 9, 12]
 ]
 function Board () {
   const [board, setBoard] = React.useState(initialBoard)
