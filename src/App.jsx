b743f806470a6182e8e0eec77ec389d2e3a7eb90
Yosber Guerrero <yosbergc@gmail.com>
2024-04-30 22:07:51 -0400

    4-in-a-line starter file

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Board } from "./components/Game/Board"
+function App() {
+  return (
+    <section className="main-container">
+      <Board />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/Game/Board.jsx
@@ -0,0 +1,23 @@
+import './board.css'
+function Board () {
+
+    return (<section className="board">
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+        <div className='box'>x</div>
+    </section>)
+}
+export { Board }
